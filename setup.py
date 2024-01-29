68c1cbcc95ad2c1bb45c52b9c3a708f711cf3883
Matías Galetovic <matias.galetovic@toesca.cl>
2024-01-29 15:47:15 -0300

    Correct install_requires in setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='tpower',
-    version='0.1.0',
+    version='0.1.3',
     packages=find_packages(),
     include_package_data=True,
-    description='Python package by T-Power.'
+    description='Python package by T-Power.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Matías Galetovic Streeter',
     author_email='matias.galetovic@toesca.com',
     url='https://github.com/toesca-dev/tpower/',
     install_requires=[
-        'requests', 'logging', 'pandas', 'datetime', 'dateutils'
+        'requests', 'pandas', 'numpy'
     ],
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
